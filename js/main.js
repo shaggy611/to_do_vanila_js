c06998a4b1419333332f4e9893d233f439db588b
shaggy611 <sevczik11@gmail.com>
2019-11-13 15:14:38 +0200

    generate_random_id + unactive_item_by_clicking_checkbox

--- js/main.js
+++ js/main.js
@@ -12,46 +12,57 @@ $(document).ready(function () {
 				$('.insert_new_note').append(note_block.responseText);
 				/*====================================================================*/
 				/*Beyond is a part of a code which calculate number of note items.*/
-				/*let note_list_item_field_length = $(".note_list_item_main .note_list_item_field").length+1;
-                console.log(note_list_item_field_length);*/
-				let note_list_item_field_length = $(".insert_new_note .col_padd_down").length;
-				/*for (let i = 1; i <= note_list_item_field_length; i++) {
-					$(".item:first").addClass("item" + i).removeClass("item");
-		
-				}*/
-				console.log(note_list_item_field_length);
-				/*====================================================================*/
+				let note_list_item_field_length = $(".insert_new_note .col_padd_down").last();
+				let uuid = `f${(+new Date).toString(16) + Math.random()}`;
+				note_list_item_field_length.attr('id', uuid);
+				/*========================================================================*/
 			});
-/*============================================================================*/	
-
-
-	
-	
-	
-	
-
+/*========================================================================================*/	
 
 	
 	
 	
-	/*$('.note_list_item_js').keypress(function(event){
-		if(event.keyCode == 13){
-			let note_list_item_js = $('.note_list_item_js').val();
-			$('.note_list_item_field_js').append('<div class="item checkbox"><input type="checkbox" tabindex="0"><label class="chbx_style">' + note_list_item_js + '</label></div>');
-			$('.note_list_item_js').val('');
-		};
-	});
-
-   
-	$('.clear').click(function(){
-		$('.note_list_item_field_js').empty();
-	});*/
+	let CheckBoxActivated  = document.querySelector('.insert_new_note');
+	let checkBoxMakeNoteItemInactive = CheckBoxActivated.addEventListener('click', CBactivated);
+	function CBactivated(event) {
+		let which_elem_user_click = event.target;
+		let IdOfInput = which_elem_user_click.id;
+		
+		if (which_elem_user_click.checked == true) {
+			$(`#${IdOfInput}`).next().addClass('chbx_style inactive_text');
+		}
+		else if (which_elem_user_click.checked == false) {
+				 $(`#${IdOfInput}`).next().removeClass('inactive_text');
+				 }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
     let addClickListenHere = document.querySelector('#addClickListenHere');
 	let note_list_item_listen = addClickListenHere.addEventListener('click', editCurrentElem);
 	
 	
 	function editCurrentElem(event) {
 		let which_elem_user_click = event.target;
+		
+		let a = which_elem_user_click.parentNode;
+		console.log(a);
+		
 		
 		
 		if (which_elem_user_click.className == "ui button clear") {
@@ -67,20 +78,7 @@ $(document).ready(function () {
 			addClassForInpFld[0].className = 'ui list note_list_item_field note_list_item_field_js';
             console.log(addClassForInpFld);	
 		}
-		
-		/*$('.note_list_item_js').keypress(function (event) {
-				if (event.keyCode == 13) {
-
-					let note_list_item_js_1 = $('.note_list_item_js').val();
-
-					$('.note_list_item_field_js').append('<div class="item checkbox"><input type="checkbox" tabindex="0"><label class="chbx_style">' + note_list_item_js_1 + '</label></div>');
-					$('.note_list_item_js').val('');
-					
-					
-
-				}
-				
-		});*/
+	
 		
 		
 		
@@ -102,24 +100,36 @@ $(document).ready(function () {
 	};
 	
 	
-/*	document.addEventListener('keypress', sdfg);
-	function sdfg (event){
-		if ( event.which == 13 ) {
-		let note_list_item_js_1 = $('.note_list_item_js').val();
 
-					$('.note_list_item_field_js').append('<div class="item checkbox"><input type="checkbox" tabindex="0"><label class="chbx_style">' + note_list_item_js_1 + '</label></div>');
-					$('.note_list_item_js').val('');
-	}
-		else return;
-	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	$(document).keypress(function (event) {
 				if (event.keyCode == 13) {
-
+					
+					
+					
+const uuid = `f${(+new Date).toString(16)}`;
+					
+					
+					
 					let note_list_item_js_1 = $('.note_list_item_js').val();
 
-					$('.note_list_item_field_js').append('<div class="item checkbox"><input type="checkbox" tabindex="0"><label class="chbx_style">' + note_list_item_js_1 + '</label></div>');
+					$('.note_list_item_field_js').append('<div class="item checkbox"><input type="checkbox" tabindex="0" class="chbx" id="' + uuid + '"><label class="chbx_style">' + note_list_item_js_1 + '</label></div>');
 					$('.note_list_item_js').val('');
 					
 					
@@ -170,7 +180,7 @@ $(document).ready(function () {
 	
 	
 	
-	
+	  
 	
 	
 	
