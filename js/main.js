651bf6de6c2b6984c11d934712c1912b6e7ae5c7
shaggy611 <sevczik11@gmail.com>
2020-07-29 22:28:03 +0300

    add_date_of_creating_note

--- js/main.js
+++ js/main.js
@@ -52,12 +52,6 @@ $(document).ready(function () {
 	/*=========================================================================================*/
 	function editCurrentElem(event) {
 		let which_elem_user_click = event.target;
-		
-			/*event.target.onblur = function () {
-				event.target.placeholder = event.target.value;
-			};*/
-		
-		
 		/*============================================================================================*/
 		/*========This little part make current note with shadow when user clicked on it/ ============*/
 		which_elem_user_click.closest('.note_list_item_main').className = 'ui raised segment note_list_item_main beautiful';
@@ -75,7 +69,7 @@ $(document).ready(function () {
 			save_note_to_local_storage();
 		};
 		/*==========Delete current note===================================================================*/
-		if (event.target.className == "ui button delete_note") {
+		if (event.target.className == "fas fa-times delete_note") {
 			let note_list = which_elem_user_click.parentElement.parentElement.parentElement;
 			note_list.remove();
 			save_note_to_local_storage();
@@ -84,6 +78,9 @@ $(document).ready(function () {
 		if (event.target.className == "ui button save_note") {
 			let xx = event.target.parentNode.previousElementSibling.firstElementChild;
 			xx.placeholder = xx.value;
+				let cc = event.target.parentElement.previousElementSibling.firstElementChild.nextElementSibling;
+				cc.innerHTML = '';
+				cc.innerHTML =  get_note_create_date();
 			save_note_to_local_storage();
 		};
 /*This part of a code operate with classes of elements input note and items note. When EventListener detect that user click out of input note or item note, function remove name of classes by which JS collaborate with HTML tags*/
@@ -126,5 +123,42 @@ $(document).ready(function () {
 		localStorage.clear();
 		localStorage.setItem("local_storage_content", local_storage_content);
 	};
+
+	function get_note_create_date() {
+		let currentDate = new Date();
+		let day = currentDate.getDate();
+		let month = currentDate.getMonth();
+		let year = currentDate.getFullYear();
+		let hours = currentDate.getHours();
+		let minutes;
+
+		function getMonthName() {
+		let month_list = new Array();
+		month_list[0] = "January";
+		month_list[1] = "February";
+		month_list[2] = "March";
+		month_list[3] = "April";
+		month_list[4] = "May";
+		month_list[5] = "June";
+		month_list[6] = "July";
+		month_list[7] = "August";
+		month_list[8] = "September";
+		month_list[9] = "October";
+		month_list[10] = "November";
+		month_list[11] = "December";
+		let month_name = month_list[month];
+		return month_name;
+		}
+
+		if (currentDate.getHours() < 10) {
+			minutes = "0" + currentDate.getMinutes();
+			console.log("yes");
+		}
+		else {minutes = currentDate.getMinutes()}
+
+		return (day + "-" + getMonthName() + "-" + year + "    " + hours + ":" + minutes);
+	}
 	/*=================================================================================*/
+
+	$(".animation_appear").transition('slide down');
 });
